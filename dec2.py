8d7e6b62b5f354f7766cff0f9a8f755a6f37dae9
Tomoya <effgrgyggf80@gmail.com>
2020-08-31 13:51:41 +0900

    test

--- dec2.py
+++ dec2.py
@@ -5,13 +5,8 @@ import os
 import time,sys
 import warnings
 import getpass
-import configparser
 import zipfilejpn
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-chk=os.path.isfile('acc.ini')
-#configparser.InterpolationSyntaxError
-k=' 設定ファイルが見つかりません'
 warnings.simplefilter('ignore')
 chef=1
 sys1=1
@@ -40,7 +35,9 @@ def pyzip(file):
         filename=input(' 圧縮対象ファイル名 >> ')
     else:
         filename=file
-    filename = filename.replace("\"","")
+    filename=filename.replace(' ', '')
+    filename=filename.replace('\'', ' ')
+    filename = filename.strip()
     name,ext = os.path.splitext(filename)
     if ext==".lnk":
         print(' 対応していないファイル形式です。')
@@ -52,17 +49,18 @@ def pyzip(file):
         print('')
         input(' メニュー画面に戻るにはエンターキーを押してください')
         return
+    sf=os.path.dirname(filename)
+    sf=sf+"/"
+    basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
+    basename_without_ext=basename_without_ext+".dec"
+    basefiles=os.path.basename(filename) 
+    os.chdir(sf)
     d=os.path.isfile(filename)
     if d!=True:
         print('ファイルが存在しません...')
         print('')
         input(' メニュー画面に戻るにはエンターキーを押してください')
         return
-    sf=os.path.dirname(filename)
-    basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-    basename_without_ext=basename_without_ext+".dec"
-    basefiles=os.path.basename(filename) 
-    os.chdir(sf)
     try :
         # 圧縮関係(英数字以外のファイル名もちゃんと圧縮できる)
         pyminizip.compress(basefiles.encode('cp932'),"\\".encode('cp932'),basename_without_ext.encode('cp932'),pass2,int(9))
@@ -90,8 +88,11 @@ def openzip(file,chef):
         filename=input(' 展開ファイル名 >> ')
     else:
         filename=file
-    filename = filename.replace("\"","")
+    filename=filename.replace(' ', '')
+    filename= filename.replace('\'', ' ')
+    filename = filename.strip()
     sf=os.path.dirname(filename)
+    sf=sf+'/'
     ext = os.path.splitext(filename)[1]
     name=os.path.splitext(os.path.basename(filename))[0]
     ext=str(ext)
@@ -154,6 +155,8 @@ def openzip(file,chef):
         print(' 出力先フォルダ: '+sf)
         time.sleep(5)
 # メニュー表示
+
+# メニュー表示
 c=-1
 sys1=int(sys1)
 if sys1==1:
@@ -179,7 +182,7 @@ if sys1==1:
 
 zipfilepointer=0
 while True:
-    os.system('cls')
+    os.system('clear')
     print('')
     print('')
     print(' ******パスワードZIP圧縮ツール　Ver 4.0******')
@@ -187,108 +190,6 @@ while True:
     print(' 0:ファイルのパスワードZIP圧縮')
     print('')
     print(' 1:パスワードZIPファイルの展開')
-    print('')
-    print(' 2:config.ini再読み込み')
-    print('')
-    print(' 終了するには[N]を入力してください')
-    print('')
-    print(' ************')
-    print('')
-    c= input(' 操作ID (0 or 1) >> ')
-    if c!="N" and c!="n":
-        try:
-            c=int(c)
-        except ValueError:
-            pass
-    #os.system('cls')
-    if c==0:
-        file=""
-        pyzip(file)
-        continue
-    if c==1:
-        file=""
-        openzip(file,chef)
-        continue
-    if c=="N" or c=="n":
-        print('')
-        print(' プログラムを終了しています。')
-        time.sleep(5)
-        sys.exit()
-        print('')#g
-        print(ext);knh
-        input(' メニュー画面に戻るにはエンターjキーを押してください')w
-        return
-    basename_without_ext = os.path.splitext(os.path.basename(filename))[0]
-    try:
-        print('')
-        print('')
-        chef=int(chef)
-        if chef==1:
-            k=input(' 続行しますか? ( Y or N )')
-        else:
-            k="y"
-        if k=="Y" or k=="YES" or k=="y":
-            print(' 展開中　しばらくお待ちください。(ファイルサイズによっては時間がかかる場合があります)')
-            zipfilepointer=zipfilejpn.ZipFile(name,"r")# ここから展開関係
-            zipfilepointer.extractall(sf,pwd=bytes(pass2))
-            zipfilepointer.close() # 展開関係ここで終わり
-        if k=="N" or k=="n" or k=="NO" or k=="no" or k=="":
-            print('')
-            print(' 展開をキャンセルしました。')
-            print('')
-            time.sleep(5)
-            return
-    except RuntimeError:
-        print(' パスワードエラー: パスワードが間違っています。')
-        print('')
-        input(' メニュー画面に戻るにはエンターキーを押してください')
-        return
-    except PermissionError:
-        print(' エラー :アクセスが拒否されたため圧縮ファイルを展開できませんでした。')
-        print('')
-        input(' メニュー画面に戻るにはエンターキーを押してください')
-        return
-    else:
-        print(' 展開が完了しました')
-        print('')
-        print(' 出力先フォルダ: '+sf)
-        time.sleep(5)
-# メニュー表示
-c=-1
-sys1=int(sys1)
-if sys1==1:
-    args = sys.argv
-    count=len(args)
-    if count==2:
-        file1=args[-1]u
-        file=args[-1]
-        ext = os.path.splitext(file1)[1]
-        if ext==".zip":
-            print('')
-            print(' エラー：zipファイルの展開と圧縮には対応していません。')
-            print('')
-            input(' 続けるにはエンターキーを押してください')
-        elif ext==".dec":
-            openzip(file,chef)
-            file=""
-        elif ext!=".zip" or ext!="dec":
-            pyzip(file)
-            file=""
-    else:
-        file=""
-
-zipfilepointer=0
-while True:
-    os.system('cls')
-    print('')
-    print('')
-    print(' ******パスワードZIP圧縮ツール　Ver 4.0******')
-    print('')
-    print(' 0:ファイルのパスワードZIP圧縮')
-    print('')
-    print(' 1:パスワードZIPファイルの展開')
-    print('')
-    print(' 2:config.ini再読み込み')
     print('')
     print(' 終了するには[N]を入力してください')
     print('')
--- xcode.py
+++ xcode.py
@@ -8,12 +8,12 @@ def xor_encrypt(plaintext, key):
 def xencode(plaintext):
     key=plaintext
     pass2=str(key)
-    pass2=hashlib.shaXXX(pass2.encode()).hexdigest()
+    pass2=hashlib.sha256(pass2.encode()).hexdigest()
     return pass2
   
 def xdecode(pass3):
     key=pass3
     pass2=str(key)
-    pass2=hashlib.shaXXX(pass2.encode()).hexdigest()
+    pass2=hashlib.sha256(pass2.encode()).hexdigest()
     pass2=bytes(pass2,encoding = "utf-8")
     return pass2
